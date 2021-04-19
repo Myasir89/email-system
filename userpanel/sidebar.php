dc9c538b118e884fb0d4cd4d8e4ba9fd6b1b09bc
Muhammad Yasir <muhammad.yasir@innovadeltech.com>
2021-04-20 00:29:10 +0500

    first commit

---
+++ userpanel/sidebar.php
@@ -0,0 +1,15 @@
+<div id="sidebar-nav" class="sidebar">
+			<div class="sidebar-scroll">
+				<nav>
+					<ul class="nav">
+						<li><a href="index.php" class="active"><i class="lnr lnr-home"></i> <span>Dashboard</span></a></li>
+						<li><a href="inbox.php" class=""><i class="lnr lnr-code"></i> <span>inbox</span></a></li>
+						<li><a href="composeemail.php" class=""><i class="lnr lnr-chart-bars"></i> <span>send Email</span></a></li>
+						<li><a href="spam.php" class=""><i class="lnr lnr-cog"></i> <span>Spam</span></a></li>
+						<li><a href="notifications.php" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
+						<li><a href="profile.php" class=""><i class="lnr lnr-dice"></i> <span>Profile</span></a></li>
+						<li><a href="contacts.php" class=""><i class="lnr lnr-text-format"></i> <span>Contacts</span></a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
